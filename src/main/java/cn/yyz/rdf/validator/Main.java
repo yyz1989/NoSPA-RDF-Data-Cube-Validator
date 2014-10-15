0bd5d338e0f1eb2e931754ccae70c7db00a10a36
Yang Yuanzhe <yyz1989@hotmail.com>
2014-10-15 07:50:47 -0700

    Added implementation of IC 5,6 with abstracted functions

--- src/main/java/cn/yyz/rdf/validator/Main.java
+++ src/main/java/cn/yyz/rdf/validator/Main.java
@@ -10,7 +10,7 @@ public class Main {
         long t1 = System.currentTimeMillis();
         //validator.checkConstraint("IC19");
         long t2 = System.currentTimeMillis();
-        validator.checkIC20();
+        validator.checkIC20_21();
         long t3 = System.currentTimeMillis();
         System.out.println(t2 - t1);
         System.out.println(t3 - t2);
