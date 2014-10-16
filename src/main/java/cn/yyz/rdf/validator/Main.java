5a9d0c42057ba13f3fbbe3f745517f54b88b0e2d
Yang Yuanzhe <yyz1989@hotmail.com>
2014-10-16 15:01:55 -0700

    Removed the first implementation of IC 3-5 after performance test

--- src/main/java/cn/yyz/rdf/validator/Main.java
+++ src/main/java/cn/yyz/rdf/validator/Main.java
@@ -9,8 +9,9 @@ public class Main {
         validator.normalize();
         long t1 = System.currentTimeMillis();
         //validator.checkConstraint("IC19");
+        validator.checkIC6();
         long t2 = System.currentTimeMillis();
-        validator.checkIC20_21();
+        validator.checkIC6_2();
         long t3 = System.currentTimeMillis();
         System.out.println(t2 - t1);
         System.out.println(t3 - t2);
