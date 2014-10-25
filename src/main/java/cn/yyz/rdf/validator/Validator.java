635a3ff8466c919748b70639a9b7ac714be89304
Yang Yuanzhe <yyz1989@gmail.com>
2014-10-25 16:49:18 -0700

    Created functions for normalization phase 2

--- src/main/java/cn/yyz/rdf/validator/Validator.java
+++ src/main/java/cn/yyz/rdf/validator/Validator.java
@@ -4,6 +4,7 @@ import com.hp.hpl.jena.query.*;
 import com.hp.hpl.jena.rdf.model.*;
 import com.hp.hpl.jena.update.UpdateAction;
 import com.hp.hpl.jena.util.FileManager;
+import com.hp.hpl.jena.vocabulary.RDF;
 
 import java.io.InputStream;
 import java.util.*;
@@ -31,7 +32,7 @@ public class Validator {
         model.write(System.out, "TURTLE");
     }
 
-    public void normalize() {
+    public void normalizePhase1() {
         // Phase 1: Type and property closure
         NodeIterator nodeIterator = model.listObjectsOfProperty(QB_observation);
         while (nodeIterator.hasNext()) {
@@ -70,11 +71,118 @@ public class Validator {
             model.add(statement.getSubject(), QB_componentProperty, statement.getObject());
             model.add(statement.getObject().asResource(), RDF_type, QB_AttributeProperty);
         }
+    }
 
-        // Phase 2: Push down attachment levels
-        String queryString = NormalizationAlgorithm.PHASE2.getValue();
-        UpdateAction.parseExecute(queryString, model);
+    public void normalizePhase2() {
+        pushDownDatasetAttachments();
+        pushDownSliceAttachments();
+        pushDownDimValOnSlice();
+    }
 
+    private void pushDownDatasetAttachments() {
+        Map<Resource, Set<? extends RDFNode>> specSetByDataset = searchByPathVisit(null,
+                Arrays.asList(QB_structure, QB_component), null);
+        Map<Property, RDFNode> objByProp = new HashMap<Property, RDFNode>();
+        objByProp.put(QB_componentAttachment, QB_DataSet);
+        Map<Resource, Map<Property, Set<RDFNode>>> compBySpec = searchByChildProperty(null,
+                objByProp, Arrays.asList(QB_componentProperty));
+        for (Resource dataset : specSetByDataset.keySet()) {
+            Set<Resource> obsSet = model.listSubjectsWithProperty(QB_dataSet, dataset).toSet();
+            Set<? extends RDFNode> specSet = specSetByDataset.get(dataset);
+            specSet.retainAll(compBySpec.keySet());
+            Set<RDFNode> compSet = new HashSet<RDFNode>();
+            for (RDFNode spec : specSet) {
+                compSet.addAll(compBySpec.get(spec.asResource()).get(QB_componentProperty));
+            }
+            Map<Property, Set<RDFNode>> valueSetByComp = new HashMap<Property, Set<RDFNode>>();
+            for (RDFNode comp : compSet) {
+                if (comp.isURIResource()) {
+                    Property compAsProp =
+                            ResourceFactory.createProperty(comp.asResource().getURI());
+                    valueSetByComp.put(compAsProp,
+                            model.listObjectsOfProperty(dataset, compAsProp).toSet());
+                }
+            }
+            insertValueToObs(obsSet, valueSetByComp);
+        }
+    }
+
+    private void pushDownSliceAttachments() {
+        Map<Resource, Set<? extends RDFNode>> specSetByDataset = searchByPathVisit(null,
+                Arrays.asList(QB_structure, QB_component), null);
+        Map<Property, RDFNode> objByProp = new HashMap<Property, RDFNode>();
+        objByProp.put(QB_componentAttachment, QB_Slice);
+        Map<Resource, Map<Property, Set<RDFNode>>> compBySpec = searchByChildProperty(null,
+                objByProp, Arrays.asList(QB_componentProperty));
+        for (Resource dataset : specSetByDataset.keySet()) {
+            Set<? extends RDFNode> specSet = specSetByDataset.get(dataset);
+            specSet.retainAll(compBySpec.keySet());
+            Set<RDFNode> compSet = new HashSet<RDFNode>();
+            for (RDFNode spec : specSet) {
+                compSet.addAll(compBySpec.get(spec.asResource()).get(QB_componentProperty));
+            }
+            NodeIterator sliceIter = model.listObjectsOfProperty(dataset, QB_slice);
+            Set<Resource> sliceSet = nodeToResource(sliceIter.toSet());
+            for (Resource slice : sliceSet) {
+                NodeIterator resIter = model.listObjectsOfProperty(slice, QB_observation);
+                Set<Resource> obsSet = nodeToResource(resIter.toSet());
+                Map<Property, Set<RDFNode>> valueSetByComp = new HashMap<Property, Set<RDFNode>>();
+                for (RDFNode comp : compSet) {
+                    if (comp.isURIResource()) {
+                        Property compAsProp =
+                                ResourceFactory.createProperty(comp.asResource().getURI());
+                        valueSetByComp.put(compAsProp,
+                                model.listObjectsOfProperty(slice, compAsProp).toSet());
+                    }
+                }
+                insertValueToObs(obsSet, valueSetByComp);
+            }
+        }
+    }
+
+    private void pushDownDimValOnSlice () {
+        Map<Resource, Set<? extends RDFNode>> specSetByDataset = searchByPathVisit(null,
+                Arrays.asList(QB_structure, QB_component), null);
+        Map<Resource, Set<? extends RDFNode>> compBySpec = searchByPathVisit(null,
+                Arrays.asList(QB_componentProperty), null);
+        Set<Resource> compWithDef = model.listResourcesWithProperty(RDF_type,
+                QB_DimensionProperty).toSet();
+        for (Resource dataset : specSetByDataset.keySet()) {
+            Set<? extends RDFNode> specSet = specSetByDataset.get(dataset);
+            specSet.retainAll(compBySpec.keySet());
+            Set<RDFNode> compSet = new HashSet<RDFNode>();
+            for (RDFNode spec : specSet) {
+                compSet.addAll(compBySpec.get(spec.asResource()));
+            }
+            compSet.retainAll(compWithDef);
+            NodeIterator sliceIter = model.listObjectsOfProperty(dataset, QB_slice);
+            Set<Resource> sliceSet = nodeToResource(sliceIter.toSet());
+            for (Resource slice : sliceSet) {
+                NodeIterator resIter = model.listObjectsOfProperty(slice, QB_observation);
+                Set<Resource> obsSet = nodeToResource(resIter.toSet());
+                Map<Property, Set<RDFNode>> valueSetByComp = new HashMap<Property, Set<RDFNode>>();
+                for (RDFNode comp : compSet) {
+                    if (comp.isURIResource()) {
+                        Property compAsProp =
+                                ResourceFactory.createProperty(comp.asResource().getURI());
+                        valueSetByComp.put(compAsProp,
+                                model.listObjectsOfProperty(slice, compAsProp).toSet());
+                    }
+                }
+                insertValueToObs(obsSet, valueSetByComp);
+            }
+        }
+    }
+
+    private void insertValueToObs(Set<Resource> obsSet, Map<Property, Set<RDFNode>> objSetByProp) {
+        for (Property prop : objSetByProp.keySet()) {
+            Set<RDFNode> objSet = objSetByProp.get(prop);
+            for (Resource obs : obsSet) {
+                for (RDFNode obj : objSet) {
+                    model.add(obs, prop, obj);
+                }
+            }
+        }
     }
 
     public void checkICAll() {
